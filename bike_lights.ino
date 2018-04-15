b9d68b71eef8dc74921040f2539afbe1a4574a87
Kristian Kann <arrvi@arrvi.eu>
2018-04-15 22:17:39 +0200

    Constants - thats something

---
+++ bike_lights.ino
@@ -0,0 +1,14 @@
+#define OUT_LEFT 3
+#define OUT_RIGHT 5
+#define OUT_READ 6
+#define OUT_HEAD1 9
+#define OUT_HEAD2 10
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
